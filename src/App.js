cd61d54d43bbe7451519dd410b46725909a38816
Gregory Petrov <grishap96123@gmail.com>
2022-08-20 22:22:14 +0300

    added calculator page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Routes, Route, Link } from "react-router-dom";
+import {Routes, Route, Link} from "react-router-dom";
 import LoginPage from "./views/login-page/LoginPage";
+import Calculator from "./views/calculator/Calculator";
 
 const App = () => {
     return (
         <div>
             <Routes>
-                <Route path="/login" element={<LoginPage/>} />
-                <Route path="/calculator" element={null} />
+                <Route path="/login" element={<LoginPage/>}/>
+                <Route path="/" element={<Calculator/>}/>
+                <Route path="/history" element={null}/>
             </Routes>
         </div>
     );
---
+++ src/views/calculator/Calculator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Calculator.scss'
+
+const Calculator = () => {
+    return (
+        <div className={'calculator'}>
+
+        </div>
+    );
+};
+
+export default Calculator;
