96dfc70a86399f9ac9c33a1d642f8f0a79dc5159
Gregory Petrov <grishap96123@gmail.com>
2022-08-20 23:17:17 +0300

    added first buttons to body

--- src/utils/service.js
+++ src/utils/service.js
@@ -2,7 +2,7 @@ import {USER_EMAIL, USER_NAME} from "../views/login-page/LoginPage";
 
 export const checkLoginStatus = (navigate) => {
     if (!!localStorage.getItem(USER_NAME) && !!localStorage.getItem(USER_EMAIL)) {
-        navigate('/calculator');
+        navigate('/');
     } else {
         navigate('/login');
     }
--- src/views/calculator/components/body/Body.js
+++ src/views/calculator/components/body/Body.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import './Body.scss'
 const Body = () => {
     return (
-        <div>
-            
+        <div className={'calc-body'}>
+            <div className={'row'}>
+                <div className={'item'}><span>7</span></div>
+                <div className={'item'}><span>8</span></div>
+                <div className={'item'}><span>9</span></div>
+                <div className={'item'}><span>*</span></div>
+            </div>
         </div>
     );
 };
