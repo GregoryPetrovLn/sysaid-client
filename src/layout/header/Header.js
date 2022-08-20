f33f219597265ae5934246679005983adabde7f5
Gregory Petrov <grishap96123@gmail.com>
2022-08-20 22:56:19 +0300

    added header controllers

--- src/layout/header/Header.js
+++ src/layout/header/Header.js
@@ -3,21 +3,28 @@ import './Header.scss'
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import {useNavigate} from "react-router-dom";
+import {USER_EMAIL, USER_NAME} from "../../views/login-page/LoginPage";
 
 const Header = () => {
     const navigate = useNavigate();
+
+    const logout = () => {
+        localStorage.removeItem(USER_NAME);
+        localStorage.removeItem(USER_EMAIL);
+        navigate('/login')
+    }
 
     return (
         <div className={'app-header'}>
             <div className={'nav-btns'}>
                 <ButtonGroup variant="contained" aria-label="outlined primary button group">
-                    <Button onClick={()=>navigate('/')}>Calculator</Button>
-                    <Button onClick={()=>navigate('/history')}>History</Button>
+                    <Button onClick={() => navigate('/')}>Calculator</Button>
+                    <Button onClick={() => navigate('/history')}>History</Button>
                 </ButtonGroup>
-            </div>
-
-            <div className={'login-status'}>
-
+                <div className={'login-status'}>
+                    <Button onClick={logout}>Logout</Button>
+                    <div className={'user-name'}>Hello {localStorage.getItem(USER_NAME)}</div>
+                </div>
             </div>
         </div>
     );
