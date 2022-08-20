480a4d88e11260037e86ba3ad16db69bc383ab32
Gregory Petrov <grishap96123@gmail.com>
2022-08-20 20:59:24 +0300

    added basic files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {applyMiddleware, createStore} from "redux";
+import {composeWithDevTools} from "redux-devtools-extension";
+import {Provider} from "react-redux";
+import {rootReducer} from "./redux/rootReducer";
+import thunk from "redux-thunk";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
